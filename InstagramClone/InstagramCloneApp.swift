cf2c59797a2039dcb34e60c566cabf503c16d7a0
ianjhill <ianjhill1@gmail.com>
2023-02-03 14:42:21 -0500

    Initial Commit

---
+++ InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by Ian Hill on 2/3/23.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
